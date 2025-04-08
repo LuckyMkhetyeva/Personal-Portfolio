7feefa05e162d6f67319c8e70ea8d2dc9634ea3b
LuckyMkhetyeva <mkhetyevalucky@gmail.com>
2025-04-08 13:35:49 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+// script.js
+
+function showSection(sectionId) {
+    document.querySelectorAll('.section').forEach(section => {
+        section.style.display = 'none';
+    });
+    document.getElementById(sectionId).style.display = 'block';
+}
